8a9e6b4ba188a3153e2f88a1d1753a66d085187d
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-03-29 02:24:22 -0700

    Link used to setup routes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <div>
       <Navbar bg='dark' variant='dark' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic=navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='cart'>
-                <i class='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i class='fas fa-user'></i> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i class='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i class='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
-        <Card.img src={product.image} variant='top' />
-      </a>
-      <Card.body>
-        <a href={`/product/${product._id}`}>
-          <Card.title as='div'>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
             <strong>{product.name}</strong>
-          </Card.title>
-        </a>
-        <Card.text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
-        </Card.text>
-        <Card.text as='h3'>${product.price}</Card.text>
-      </Card.body>
+          </Card.Title>
+        </Link>
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews `}
+          />
+        </Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
     </Card>
   )
 }
