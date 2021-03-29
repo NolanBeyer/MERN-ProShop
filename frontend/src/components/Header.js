8a9d1269ac350913bc2fdcb244a152c959044fc3
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-03-29 14:20:05 -0700

    removed link container

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -14,10 +14,10 @@ const Header = () => {
           <Navbar.Collapse id='basic=navbar-nav'>
             <Nav className='ml-auto'>
               <Nav.Link as={Link} to='/cart'>
-                <i class='fas fa-shopping-cart'></i> Cart
+                <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
               <Nav.Link as={Link} to='/login'>
-                <i class='fas fa-user'></i> Sign In
+                <i className='fas fa-user'></i> Sign In
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
