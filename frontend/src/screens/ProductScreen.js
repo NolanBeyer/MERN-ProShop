71a76730bd2308963c583f0457e8ec6fe91b842b
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-03-30 01:40:33 -0700

    fetched data from both product and home screen from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Button, Image, ListGroup, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
-
+import axios from 'axios'
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
   return (
     <>
       <Link class='btn btn-light my-3' to='/'>
