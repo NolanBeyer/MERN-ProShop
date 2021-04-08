85d0aabc86085167530303321e1ba8e418fec50a
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-04-08 01:43:39 -0700

    dispatched addToCart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -17,7 +17,7 @@ const CartScreen = ({ match, location, history }) => {
 
   useEffect(() => {
     if (productId) {
-      dispatch(addToCart, qty)
+      dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
 
