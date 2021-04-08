0f07b4baacc9836668f1fec12e29afc6934d6ce4
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-04-07 22:10:56 -0700

    auth middleware and user routes setup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
 const router = express.Router()
+import { protect } from '../middleWare/authMiddleWare.js'
 
 router.post('/login', authUser)
+
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
