6a33d9a8567d1620d1725c64bdd79a9a375205fb
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-04-10 02:07:19 -0700

    update user profile setup and tested through postman

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,17 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js'
 const router = express.Router()
 import { protect } from '../middleWare/authMiddleWare.js'
 
 router.post('/login', authUser)
 
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 router.route('/').post(registerUser)
 
