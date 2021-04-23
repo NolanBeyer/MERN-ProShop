9fe7e11d2a8a57fd8e304024aa11b35ffdc64441
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-04-23 01:01:49 -0700

    added delte functionality to the admin user

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,6 +8,9 @@ import {
   USER_LIST_SUCCESS,
   USER_LIST_RESET,
   USER_LOGIN_REQUEST,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
 } from '../constants/userConstants'
 import {
   USER_LOGIN_SUCCESS,
@@ -84,6 +87,19 @@ export const userListReducer = (state = { users: [] }, action) => {
       return { loading: false, error: action.payload }
     case USER_LIST_RESET:
       return { users: [] }
+    default:
+      return state
+  }
+}
+
+export const userDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true }
+    case USER_DELETE_SUCCESS:
+      return { loading: false, success: true }
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
