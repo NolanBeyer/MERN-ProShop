ed6d33c9b8852ccdfb4e91d6e56d86093a572750
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-04-27 00:57:08 -0700

    added create and update product controller and route file. Tested all end points in postman

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,17 @@ import {
   getProducts,
   getProductById,
   deleteProductById,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleWare/authMiddleWare.js'
-router.route('/').get(getProducts)
+
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 router
   .route('/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProductById)
+  .put(protect, admin, updateProduct)
 
 export default router
