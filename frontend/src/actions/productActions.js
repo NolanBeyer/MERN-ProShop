709236798918ee89b9db36ab2ab15a73b0473370
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-05-02 02:15:39 -0700

    order controller and route set up. Then added orderlist constants reducer and actions. Added that to my store. Created orderListScreen and added that and added that to app js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -142,7 +142,7 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     }
 
     const { data } = await axios.put(
-      `/api/products${product._id}`,
+      `/api/products/${product._id}`,
       product,
       config
     )
