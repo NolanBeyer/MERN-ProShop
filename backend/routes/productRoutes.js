5adcbbe1812765cb710781d8b1632947a787395b
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-05-03 01:46:30 -0700

    Product review controller and route create. Has been teseted in postman as well

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ import {
   getProductById,
   deleteProductById,
   createProduct,
+  createProductReview,
   updateProduct,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleWare/authMiddleWare.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 router
   .route('/:id')
